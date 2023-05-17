7ec7fbdf6ca7d67f282162a18ad89a443bd77b2b
Maique Rosa <maique.rosa@gmail.com>
2023-05-16 21:48:17 -0300

    refact: remove urldb from code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NodeApiModule } from './node-api/node-api.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     NodeApiModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://msrinfo:kayrossxp@cluster0.sojjznn.mongodb.net/nodeb',
-    ),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => {
+        const uri = config.get('DATABASE_URL');
+        console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAA', uri);
+        return { uri };
+      },
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
