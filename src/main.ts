68570bd7cf18c67eec892b52da1ce8c45952a531
Maique Rosa <maique.rosa@gmail.com>
2023-05-18 17:44:32 -0300

    chore: removing self asign cert

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      key: readFileSync(resolve(__dirname, '../cert/key.pem')),
-      cert: readFileSync(resolve(__dirname, '../cert/certificate.pem')),
-    },
-  });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(443);
+  await app.listen(8080);
 }
 bootstrap();
