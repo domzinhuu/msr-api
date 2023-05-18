90ae291e2b75aae4550090abb2bf021ec2219e93
Maique Rosa <maique.rosa@gmail.com>
2023-05-17 21:07:51 -0300

    chore: add ssl tester in api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: readFileSync(resolve(__dirname, '../cert/key.pem')),
+      cert: readFileSync(resolve(__dirname, '../cert/certificate.pem')),
+    },
+  });
   app.enableCors();
-  await app.listen(8080);
+  await app.listen(443);
 }
 bootstrap();
